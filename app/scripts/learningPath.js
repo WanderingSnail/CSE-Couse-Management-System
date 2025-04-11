7ffaae8fa67e9ec32cd4a033db4d7160d60056b2
Yousef-ye2206988 <ye2206988@qu.edu.qa>
2025-04-11 23:46:53 +0300

    GPA

--- app/scripts/learningPath.js
+++ app/scripts/learningPath.js
@@ -1,9 +1,11 @@
 import { Student } from "./class/student.js";
 import { Class } from "./class/class.js";
+import { Course } from "./class/course.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const students = await Student.load();
   const classes = await Class.load();
+  const courses = await Course.load();
   const studentID = localStorage.getItem("id");
   const student = students.find((student) => student.id === studentID);
 
@@ -104,10 +106,51 @@ document.addEventListener("DOMContentLoaded", async () => {
     displayCompletedCourses(filteredCompleted);
   }
 
+  function getCreditHours(course) {
+    const courseDetails = courses.find((c) => c.courseId === course.courseId);
+
+    if (courseDetails) {
+      return courseDetails.creditHours;
+    } else {
+      return `Course not found.`;
+    }
+  }
+
+  function calcGPA() {
+    const gradePoints = {
+      'A': 4.00,
+      'B+': 3.50,
+      'B': 3.00,
+      'C+': 2.50,
+      'C': 2.00,
+      'D+': 1.50,
+      'D': 1.00,
+      'F': 0.00
+    };
+  
+    let totalPoints = 0;       // Σ (credit hours × grade points)
+    let totalCreditHours = 0;  // Σ credit hours
+  
+    learningPath.completed.forEach((course) => {
+      const grade = course.grade;
+      const creditHours = getCreditHours(course);  
+      const points = creditHours * gradePoints[grade];
+      totalPoints += points;
+      totalCreditHours += creditHours;
+      
+    });
+  
+    const gpa = totalCreditHours > 0 ? (totalPoints / totalCreditHours).toFixed(2) : "0.00";
+    const gpaDisplay = document.querySelector("#gpaValue");
+    if (gpaDisplay) gpaDisplay.textContent = gpa;
+  }
+  
+
   searchBtn.addEventListener("click", filterCourses);
   searchByName.addEventListener("input", filterCourses);
   searchByCategory.addEventListener("change", filterCourses);
 
   displayEnrolledCourses();
   displayCompletedCourses();
+  calcGPA();
 });
